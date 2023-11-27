aff5b0c871a863a556278c93c973c085a4cf8646
Rokibul Islam <rokibulislamalways101@gmail.com>
2023-11-27 20:26:43 +0600

    all text

--- script.js
+++
@@ -1,7 +0,0 @@
-
-// scroll sections
-            window.onscroll = () => {
-              // sticky navbar
-              let navbar = document.querySelector('navbar');
-              navbar.classList.toggle('sticky', window.scrolly > 100);
-            }
